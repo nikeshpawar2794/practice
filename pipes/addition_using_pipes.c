c39402bb8f2f328736f880d13636563460cbc7e9
nikeshpawar2794 <nikeshpawar2794@gmail.com>
2021-12-04 14:53:58 +0530

    addition_using_pipes.c updated

--- pipes/addition_using_pipes.c
+++ pipes/addition_using_pipes.c
@@ -11,15 +11,27 @@ int main(int argc, char *argv[])
     pid_t id;
     char buff[size];
     int pid1[2];
+    int pid2[2];
     pipe(pid1);
+    pipe(pid2);
+
     id = fork();
     
     if(id == 0)
     {
-	    printf("Child Process\n");
+        close(pid1[1]);	    
+        
         //read(pid1[0],buff,size);
         read(pid1[0],arr,size);
-        printf("%d %d\n",arr[0],arr[1]);  
+        printf("%d %d\n",arr[0],arr[1]); 
+        printf("Child Process\n"); 
+        close(pid1[0]);
+
+
+        close(pid2[0]);
+        sum=arr[0] + arr[1];
+        write(pid2[1],&sum,sizeof(sum));
+        close(pid2[1]);
     }
 
     else
@@ -30,8 +42,16 @@ int main(int argc, char *argv[])
         
         printf("Enter two integer's : ");
         scanf("%d %d",&arr[0],&arr[1]);
-        write(pid1[1],(int *)arr,8);
+        write(pid1[1],arr,sizeof(arr));
+       
         printf("data sent successfully \n");
+        close(pid1[1]);
+
+
+        close(pid2[1]);
+        read(pid2[0],&sum,sizeof(sum));
+        printf("Additon = %d\n",sum);
+        printf("data Received from child successfully \n");
 
     }
            return 0;
