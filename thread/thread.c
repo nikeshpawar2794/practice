26d2401f772ab09cb022777a50327e6d1df9cc4f
nikeshpawar2794 <nikeshpawar2794@gmail.com>
2021-12-06 18:06:43 +0530

    Thread demo added

--- thread/thread.c
+++ thread/thread.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
+#include <pthread.h>
+
+void *greed(void *arg)
+{
+	printf("Hello \n");
+}
+
 int main()
 {
-	printf("Thread Demo\n");
+
+pthread_t threadid;
+printf("Before thread \n");
+pthread_create(&threadid, NULL, greed, NULL);
+pthread_join(threadid,NULL);
+printf("After thread \n");
+
 	return 0;
 }
